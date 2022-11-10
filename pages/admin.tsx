88a389789c1a3dd2a0900f1bf3bf45beff724546
basttian <sebastianjof@gmail.com>
2022-11-10 17:54:34 -0300

    update_pages

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,13 +1,49 @@
 
 import Layout from "../components/layout"
+import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
+//CArd Info
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+
+import { useRouter } from 'next/router';
+import { useSession } from "next-auth/react"
 
 export default function AdminDashboard() {
-
-
+  const router = useRouter()
+  const { data: session, status } = useSession()
   return (
     <Layout loader={false}>
-      <h1>Tablero</h1>
+      <Paper sx={{ width: '100%', maxWidth: '100%' }}>
+      <MenuList>
+      <MenuItem onClick={() => router.back()}>
+        <ListItemIcon>
+          <ArrowBackIcon fontSize="small" />
+        </ListItemIcon>
+      </MenuItem>
+      </MenuList>
+      </Paper>
+      <br />
+      <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar
+            alt={session.user.name ?? session.user.name}
+            src={session.user.image ?? `${session.user.image}`}
+            sx={{ width: 56, height: 56 }}
+          />
+        }
+        title={session.user.name ?? session.user.name}
+        subheader={session.user.email ?? session.user.email}
+      />
+      </Card>
 
     </Layout>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,46 @@
 import type { NextPage } from 'next'
 import Layout from "../components/layout"
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import TableViewIcon from '@mui/icons-material/TableView';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { useRouter } from 'next/router';
+import { useSession } from "next-auth/react"
 
 const Home: NextPage = () => {
-
+  const router = useRouter()
+  const { data: session, status } = useSession()
   return (
     <>
     <Layout loader={false}>
-
       <noscript>You need to enable JavaScript to run this app.</noscript>
-
+      {session && (
+      <Paper sx={{ width: '100%', maxWidth: '100%' }}>
+      <MenuList>
+      <MenuItem onClick={() => router.push('/notas')}>
+        <ListItemIcon>
+          <TableViewIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>
+            Ir a notas
+        </ListItemText>
+      </MenuItem>
+      <Divider />
+      <MenuItem onClick={() => router.push('/admin')}>
+        <ListItemIcon>
+          <SettingsIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>
+            Administración
+        </ListItemText>
+      </MenuItem>
+      </MenuList>
+      </Paper>
+    )}
     </Layout>
     </>
   )
